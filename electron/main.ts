28d5bbb4cb0fe88ece44d86d56a36941c4485880
Caner Celik <celikcaner92@gmail.com>
2025-11-27 22:57:14 +0300

    v0.0.2: Otomatik güncelleme loglaması eklendi

--- electron/main.ts
+++ electron/main.ts
@@ -1,6 +1,11 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
+import { app, BrowserWindow, ipcMain, dialog } from 'electron';
 import path from 'path';
 import { autoUpdater } from 'electron-updater';
+import log from 'electron-log';
+
+// Loglama ayarları
+log.transports.file.level = 'info';
+autoUpdater.logger = log;
 
 // Geliştirme ortamında mı çalışıyoruz?
 const isDev = process.env.NODE_ENV === 'development';
@@ -28,6 +33,48 @@ function createWindow() {
         autoUpdater.checkForUpdatesAndNotify();
     }
 }
+
+// --- Auto Updater Events ---
+
+autoUpdater.on('checking-for-update', () => {
+    log.info('Güncelleme kontrol ediliyor...');
+});
+
+autoUpdater.on('update-available', (info) => {
+    log.info('Güncelleme mevcut:', info);
+});
+
+autoUpdater.on('update-not-available', (info) => {
+    log.info('Güncelleme yok:', info);
+});
+
+autoUpdater.on('error', (err) => {
+    log.error('Güncelleme hatası:', err);
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "İndirme hızı: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - İndirilen %' + progressObj.percent;
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    log.info(log_message);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+    log.info('Güncelleme indirildi:', info);
+    const dialogOpts = {
+        type: 'info' as const,
+        buttons: ['Yeniden Başlat', 'Daha Sonra'],
+        title: 'Uygulama Güncellemesi',
+        message: process.platform === 'win32' ? info.releaseNotes as string : info.releaseName as string,
+        detail: 'Yeni bir sürüm indirildi. Uygulamayı güncellemek için yeniden başlatın.'
+    };
+
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        if (returnValue.response === 0) {
+            autoUpdater.quitAndInstall();
+        }
+    });
+});
 
 app.whenReady().then(createWindow);
 
